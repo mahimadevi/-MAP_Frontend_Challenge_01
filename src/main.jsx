4189480623d8e3fc079718022bb5cab2b6ed400d
Mahima Devi <72203327+mahimadevi@users.noreply.github.com>
2024-06-06 22:43:28 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+
+const esbuildOptions = {
+  entryPoints: ["src/main.jsx"],
+  bundle: true,
+  outdir: "dist",
+  loader: {
+    ".js": "jsx",
+    ".jsx": "jsx",
+    // Add other file types as needed
+  },
+};
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+  document.getElementById("root")
+);
